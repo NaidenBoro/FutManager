b87274270b363d3d16b06781b72f1a47b128999f
HristoKanev <icko_kanev@abv.bg>
2022-02-27 15:22:27 +0200

    added club button view and controller

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FutManager.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
