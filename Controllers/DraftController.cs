0a729bc3bb0429c9cb2b2a35773341b4ef1a705c
NanchoBG <67338983+NanchoBG@users.noreply.github.com>
2022-03-06 12:28:52 +0200

    small changes to drafts

---
+++ Controllers/DraftController.cs
@@ -0,0 +1,14 @@
+﻿using FutManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutManager.Controllers
+{
+    public class DraftController : Controller
+    {
+        public IActionResult Index()
+        {
+            List <Draft> list = new List<Draft>();
+            return View(list);
+        }
+    }
+}
--- Models/Draft.cs
+++ Models/Draft.cs
@@ -6,28 +6,36 @@
         public string Creator { get; set; }
         //Type - 1 for only real playes, 2 for only fake players, 3 with both fake and real
         public int Type { get; set; }
+        public int GoalkeeperId { get; set; }
         public Player? Goalkeeper { get; set; }
+        public int LeftDefenderId { get; set; }
         public Player? LeftDefender { get; set; }
+        public int RightDefenderId { get; set; }
         public Player? RightDefender { get; set; }
+        public int LeftMidfielderId { get; set; }
         public Player? LeftMidfielder { get; set; }
+        public int RightMidfielderId { get; set; }
         public Player? RightMidfielder { get; set; }
+        public int LeftForwardId { get; set; }
         public Player? LeftForward { get; set; }
+        public int RightForwardId { get; set; }
         public Player? RightForward { get; set; }
+        public int ManagerId { get; set; }
         public Manager? Manager { get; set; }
 
-        public Draft(string name,string creator,int type, Player goalk,Player LDef,Player RDef,Player LMid,Player RMid,Player LForw,Player RForw, Manager manager)
+        public Draft(string name,string creator,int type, int goalk,int LDef,int RDef,int LMid,int RMid,int LForw,int RForw, int manager)
         {
             Name = name;
             Creator = creator;
             Type = type;
-            Goalkeeper = goalk;
-            LeftDefender = LDef;
-            RightDefender = RDef;
-            LeftMidfielder = LMid;
-            RightMidfielder = RMid;
-            LeftForward = LForw;
-            RightForward = RForw;
-            Manager = manager;
+            GoalkeeperId = goalk;            
+            LeftDefenderId = LDef;
+            RightDefenderId = RDef;
+            LeftMidfielderId = LMid;
+            RightMidfielderId = RMid;
+            LeftForwardId = LForw;
+            RightForwardId = RForw;
+            ManagerId = manager;
 
         }
 
