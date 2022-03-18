9504645be77f6105b64c10d1e8ae47db45f584d1
MartinKaraivanov <87902145+MartinKaraivanov@users.noreply.github.com>
2022-03-18 19:31:09 +0200

    Unit testing for players

--- Testing/Player.cs
+++ Testing/Player.cs
@@ -23,11 +23,241 @@ namespace Testing
             Assert.AreEqual(1, DataService.GetPlayers().Last().Age);
             Assert.AreEqual(2, DataService.GetPlayers().Last().ShirtNumber);
             // to do
-            DataService.DeleteNation(DataService.GetPlayers().Last().Id);
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
             DataService.DeleteClub(club_id);
             DataService.DeleteNation(nat_id);
 
             //...
+        }
+
+        [Test]
+        public void DetailsShouldRedirectToErrorPageOnInvalidId()
+        {
+            PlayerController controller = new PlayerController();
+            var result = controller.Details(-1) as RedirectToActionResult;
+            Assert.AreEqual("Error", result.ControllerName);
+        }
+
+        [Test]
+        public void DetailsShouldRedirectToViewOnValidId()
+        {
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("1", "2", "Forward", 1, 2, nat_id, club_id, 1, true);
+           
+            PlayerController controller = new PlayerController();
+            var result = controller.Details(1) as ViewResult;
+            Assert.IsNotNull(result);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void AddShouldRedirectToErrorPageOnInvalidId()
+        {
+
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("1", "2", "Forward", 1, 2, nat_id, club_id, 1, true);
+
+            PlayerController controller = new PlayerController();
+
+            var result = controller.Add(null, "2", "Forward", nat_id, club_id, 1, 2, 1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.Add("1", null, "Forward", nat_id, club_id, 1, 2, 1, true);
+            Assert.AreEqual ("Error", result.ControllerName);
+
+            result = controller.Add("1", "2", null, nat_id, club_id, 1, 2, 1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.Add("1", "2", "Forward", -1, club_id, 1, 2, 1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.Add("1", "2", "Forward", nat_id, -1, 1, 2, 1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.Add("1", "2", "Forward", nat_id, club_id, -1, 2, 1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.Add("1", "2", "Forward", nat_id, club_id, 1, -1, 1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.Add("1", "2", "Forward", nat_id, club_id, 1, 2, -1, true);
+            Assert.AreEqual("Error", result.ControllerName);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+
+        }
+
+        [Test]
+        public void AddShouldCreateItem()
+        {
+
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("1", "2", "Forward", 1, 2, nat_id, club_id, 1, true);
+
+            PlayerController controller = new PlayerController();
+            controller.Add("1", "2", "Forward", nat_id, club_id, 1, 2, 1, true);
+            Assert.AreEqual("1", DataService.GetPlayers().Last().FirstName);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void EditConfirmedShouldRedirectToErrorPageOnInvalidInput()
+        {
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("1", "2", "Forward", 1, 2, nat_id, club_id, 1, true);
+
+            int id = DataService.GetPlayers().Last().Id;
+            PlayerController controller = new PlayerController();
+
+            var result = controller.ConfirmEdit(id, null, "2", "Forward", nat_id, club_id, 1, 2, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", null, "Forward", nat_id, club_id, 1, 2, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", "2", null, nat_id, club_id, 1, 2, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", "2", "Forward", -1, club_id, 1, 2, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", "2", "Forward", nat_id, -1, 1, 2, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", "2", "Forward", nat_id, club_id, -1, 2, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", "2", "Forward", nat_id, club_id, 1, -1, 1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            result = controller.ConfirmEdit(id, "1", "2", "Forward", nat_id, club_id, 1, 2, -1, true, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void EditShouldRedirectToViewOnValidId()
+        {
+
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("0", "1", "Defender", 1, 2, nat_id, club_id, 1, true);
+               
+            PlayerController controller = new PlayerController();
+            int id = DataService.GetPlayers().Last().Id;
+            controller.ConfirmEdit(id, "1", "2", "Forward", nat_id, club_id, 1, 2, 1, true, "password");
+            Assert.AreEqual("1", DataService.GetPlayers().Last().FirstName);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void EditWrongPasswordShouldNotChangeItem()
+        {
+
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("0", "1", "Defender", 1, 2, nat_id, club_id, 1, true);
+
+            PlayerController controller = new PlayerController();
+            int id = DataService.GetPlayers().Last().Id;
+            controller.ConfirmEdit(id, "1", "2", "Forward", nat_id, club_id, 1, 2, 1, true, "notpassword");
+            Assert.AreEqual("0", DataService.GetPlayers().Last().FirstName);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void RemoveWrongPasswordShouldNotChangeItem()
+        {
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("0", "1", "Defender", 1, 2, nat_id, club_id, 1, true);
+            DataService.AddPlayer("1", "2", "Midfielder", 1, 2, nat_id, club_id, 1, true);
+
+            PlayerController controller = new PlayerController();
+            int id = DataService.GetPlayers().Last().Id;
+            controller.Remove(id, "notpassword");
+            Assert.AreEqual("1", DataService.GetPlayers().Last().FirstName);
+
+            DataService.DeletePlayer(id);
+            DataService.DeletePlayer(id - 1);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void ValidRemoveShouldDeleteItem()
+        {
+
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("0", "1", "Defender", 1, 2, nat_id, club_id, 1, true);
+            DataService.AddPlayer("1", "2", "Midfielder", 1, 2, nat_id, club_id, 1, true);
+
+            PlayerController controller = new PlayerController();
+            int id = DataService.GetPlayers().Last().Id;
+            controller.Remove(id, "password");
+            Assert.AreEqual("0", DataService.GetPlayers().Last().FirstName);
+
+            DataService.DeletePlayer(id - 1);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+        }
+
+        [Test]
+        public void DeleteWithInvalidIdShouldRedirectToError()
+        {
+            DataService.AddClub("2", "2", 2);
+            DataService.AddNation("2", "2", 2);
+            int club_id = DataService.GetClubs().Last().Id;
+            int nat_id = DataService.GetNations().Last().Id;
+            DataService.AddPlayer("0", "1", "Defender", 1, 2, nat_id, club_id, 1, true);
+
+            PlayerController controller = new PlayerController();
+
+            var result = controller.Remove(-1, "password");
+            Assert.AreEqual("Error", result.ControllerName);
+
+            DataService.DeletePlayer(DataService.GetPlayers().Last().Id);
+            DataService.DeleteClub(club_id);
+            DataService.DeleteNation(nat_id);
+
         }
     }
 }
